2e776b9d0d7865d48f9e0aa8fb0dda3ff7fffa84
Sowat Tuntra-umpai <stuntra@gmail.com>
2018-12-27 10:51:38 +0700

    Remove a comment

--- src/main/java/com/ascendcorp/spring/serviceresponsetimemetrics/configuration/MetricPublisherConfig.java
+++ src/main/java/com/ascendcorp/spring/serviceresponsetimemetrics/configuration/MetricPublisherConfig.java
@@ -9,8 +9,6 @@ public class MetricPublisherConfig {
 	private static String NAMESPACE = "spring_response_time_ms";
 
     public static void publish(MetricInfo metricInfo) {
-        System.out.println("***********" + metricInfo.getEndpoint());
-        // Gauge.build().namespace("my_gauge").name("service").labelNames("service", "status_code", "isexpect").help("Hello").register(registry);
         Metrics.gauge(NAMESPACE, Tags.of("status", Integer.toString(metricInfo.getHttpStatus()), "uri", metricInfo.getEndpoint()), metricInfo.getDurationInMilliSec());
     }
 }
